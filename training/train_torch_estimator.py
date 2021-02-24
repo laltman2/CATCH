fd0680e9001cbcef0624436e514683613d4cfc47
laltman2 <laurenealtman@gmail.com>
2021-02-24 16:25:01 -0500

    loss function and dataset generation

--- training/train_torch_estimator.py
+++ training/train_torch_estimator.py
@@ -1,12 +1,14 @@
 import torch.optim as optim
 import torch.nn as nn
 from torch_entropy_arch import TorchEstimator
+from Batch_Generator import makedata
 import json
 
 with open('torch_train_config.json', 'r') as f:
     config = json.load(config)
 
 #make data here
+makedata(config)
 
 #define data loaders here
 
@@ -15,9 +17,18 @@ net.train()
 optimizer = optim.RMSprop(net.parameters())
 criterion = nn.MSELoss() 
 
-epochs = config['epochs']
+epochs = config['training']['epochs']
 
 #define loss function
+def loss_fn(outputs, labels):
+    z1, a1, n1 = outputs
+    z2, a2, n2 = labels
+
+    loss1 = criterion(z1, z2)
+    loss2 = criterion(a1, a2)
+    loss2 = criterion(n1, n2)
+
+    return loss1 + loss2 + loss3
 
 for epoch in range(epochs):
     for i, data in enumerate(trainloader, 0):
@@ -36,5 +47,5 @@ for epoch in range(epochs):
 
 print('finished training')
 
-PATH = config['save_path']
+PATH = config['training']['savefile']
 torch.save(net.state_dict(), PATH)
