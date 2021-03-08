27aa0e5504906c9bed7a1ffdb49c7d00aa100c89
laltman2 <laurenealtman@gmail.com>
2021-03-08 12:56:14 -0500

    eval accuracy with figures

--- eval/estimator_eval.py
+++ eval/estimator_eval.py
@@ -1,6 +1,7 @@
 import numpy as np
 import json, cv2, os, ast
 import pandas as pd
+import matplotlib
 from matplotlib import pyplot as plt
 from CATCH.Estimator_Torch import Estimator
 from CATCH.training.Torch_DataLoader import makedata_inner
@@ -36,11 +37,43 @@ def estimator_accuracy(configuration='test', nframes=None):
                        'z_true':params['z_p'], 'a_true': params['a_p'], 'n_true':params['n_p']}
         df = df.append(resultsdict, ignore_index=True)
 
+    print(df)
+    
     savepath = configuration + '_eval.csv'
-
     df.to_csv(savepath)
+
+    z_rmse = np.sqrt(((df.z_pred - df.z_true) **2).mean(axis=0))
+    a_rmse = np.sqrt(((df.a_pred - df.a_true) **2).mean(axis=0))
+    n_rmse = np.sqrt(((df.n_pred - df.n_true) **2).mean(axis=0))
+
+
+    matplotlib.use('TkAgg')
+    fig, axes = plt.subplots(1,3, figsize=(10,5))
+    names = ['z_p', 'a_p', 'n_p']
+    for ax, name in list(zip(axes, names)):
+        ax.grid(alpha=0.3)
+        ax.set_xlabel(r'True ${}$'.format(name))
+        ax.set_ylabel(r'Pred ${}$'.format(name))
+    ax1, ax2, ax3 = axes
+    ax1.scatter(df.z_true, df.z_pred, c='b', alpha=0.4)
+    ax1.plot(df.z_true, df.z_true, c='r')
+    ax1.annotate('RMSE = {} px'.format('%.3f'%z_rmse), xy=(0.05, 0.95), xycoords='axes fraction')
+    
+    ax2.scatter(df.a_true, df.a_pred, c='b', alpha=0.4)
+    ax2.plot(df.a_true, df.a_true, c='r')
+    ax2.annotate(r'RMSE = {}$\mu m$ '.format('%.3f'%a_rmse), xy=(0.05, 0.95), xycoords='axes fraction')
+    
+    ax3.scatter(df.n_true, df.n_pred, c='b', alpha=0.4)
+    ax3.plot(df.n_true, df.n_true, c='r')
+    ax3.annotate('RMSE = {}'.format('%.3f'%n_rmse), xy=(0.05, 0.95), xycoords='axes fraction')
+    fig.tight_layout()
+
+    figsavepath = configuration+'_eval.png'
+    fig.savefig(figsavepath)
+    
+    plt.show()
         
 
 
 if __name__ == '__main__':
-    estimator_accuracy(configuration='scale_float')
+    estimator_accuracy(configuration='scale_float2')
