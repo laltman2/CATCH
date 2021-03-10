f9c001ba27fb9be97330134b0a2d35af58e717ff
laltman2 <laurenealtman@gmail.com>
2021-03-10 16:23:20 -0500

    moved pylm to examples

--- Estimator_Torch.py
+++ Estimator_Torch.py
@@ -73,47 +73,9 @@ class Estimator(object):
 
 
 if __name__ == '__main__':
-    from matplotlib import pyplot as plt
-    import matplotlib
-    from pylorenzmie.analysis import Feature
-    from pylorenzmie.theory import LMHologram
-    from pylorenzmie.utilities import coordinates
-
     img = cv2.imread('./examples/test_image_crop.png')
     
     est = Estimator(configuration='scale_float')
     results = est.predict(img_list = [img])[0]
 
     print(results)
-
-    feature = Feature(model=LMHologram(double_precision=False))
-
-    # Instrument configuration
-    ins = feature.model.instrument
-    ins.wavelength = 0.447     # [um]
-    ins.magnification = 0.048  # [um/pixel]
-    ins.n_m = 1.34
-
-    # The normalized image constitutes the data for the Feature()
-    data = img[:,:,0]
-    data = data / np.mean(data)
-    feature.data = data
-
-    # Specify the coordinates for the pixels in the image data
-    feature.coordinates = coordinates(data.shape)
-
-    p = feature.particle
-    p.r_p = [data.shape[0]//2, data.shape[1]//2, results['z_p']]
-    p.a_p = results['a_p']
-    p.n_p = results['n_p']
-
-    holo = feature.hologram()
-    resid = feature.residuals() +1.
-
-    display = np.hstack([data, holo, resid])
-    
-    matplotlib.use('Qt5Agg')
-    plt.imshow(display, cmap='gray')
-    plt.title('Image, Predicted Holo, Residual')
-    plt.show()
-    
---
+++ examples/pylm_fit.py
@@ -0,0 +1,44 @@
+from matplotlib import pyplot as plt
+import matplotlib
+from pylorenzmie.analysis import Feature
+from pylorenzmie.theory import LMHologram
+from pylorenzmie.utilities import coordinates
+from CATCH.Estimator_Torch import Estimator
+
+img = cv2.imread('./examples/test_image_crop.png')
+
+est = Estimator(configuration='scale_float')
+results = est.predict(img_list = [img])[0]
+
+print(results)
+
+feature = Feature(model=LMHologram(double_precision=False))
+
+# Instrument configuration
+ins = feature.model.instrument
+ins.wavelength = 0.447     # [um]
+ins.magnification = 0.048  # [um/pixel]
+ins.n_m = 1.34
+
+# The normalized image constitutes the data for the Feature()
+data = img[:,:,0]
+data = data / np.mean(data)
+feature.data = data
+
+# Specify the coordinates for the pixels in the image data
+feature.coordinates = coordinates(data.shape)
+
+p = feature.particle
+p.r_p = [data.shape[0]//2, data.shape[1]//2, results['z_p']]
+p.a_p = results['a_p']
+p.n_p = results['n_p']
+
+holo = feature.hologram()
+resid = feature.residuals() +1.
+
+display = np.hstack([data, holo, resid])
+
+matplotlib.use('Qt5Agg')
+plt.imshow(display, cmap='gray')
+plt.title('Image, Predicted Holo, Residual')
+plt.show()
