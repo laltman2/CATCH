21b53f50853114d245780204c9689e470a159277
laltman2 <laurenealtman@gmail.com>
2021-06-08 11:44:39 -0400

    model timer

---
+++ eval/timer.py
@@ -0,0 +1,50 @@
+import timeit
+import numpy as np
+import pandas as pd
+
+def CATCH_timer(est_config='overlaps', loc_config='small_noisy', numimgs=200, num_trials=10, loc_dataset='../datasets/yolo_noisy/eval/', est_dataset='../datasets/overlaps/eval/'):
+
+    modelstart = '''
+from CATCH.Estimator_Torch import Estimator
+from CATCH.Localizerv5_pip import Localizer
+from CATCH.CATCHobject import CATCH
+    
+est = Estimator(configuration='{}')
+loc = Localizer(configuration='{}')
+catch = CATCH(localizer=loc, estimator = est)
+'''.format(est_config, loc_config)
+    
+    imgload = '''
+import cv2
+img_list = []
+for n in range({}):
+    img = cv2.imread('{}images/image'+str(n).zfill(4) + '.png', cv2.IMREAD_GRAYSCALE)
+    img_list.append(img)'''
+
+    est_setup = modelstart + imgload.format(numimgs, est_dataset)
+    loc_setup = modelstart + imgload.format(numimgs, loc_dataset)
+    
+    est_stmt = 'results = est.predict(img_list)'
+    loc_stmt = 'results = loc.detect(img_list)'
+    catch_stmt = 'results = catch.analyze(img_list)'
+
+    strpairs = [(loc_setup, loc_stmt), (est_setup, est_stmt), (loc_setup, catch_stmt)]
+
+    data = {'model': ['localizer', 'estimator', 'catch'],
+            'num_images': [numimgs]*3, 'num_trials': [num_trials]*3,
+            'total_time':[], 'time/trial':[], 'time/image':[]}
+    
+    for (setup, stmt) in strpairs:
+        ntot = timeit.timeit(stmt= stmt, setup= setup, number=num_trials)
+        data['total_time'].append(ntot)
+        n1 = ntot/num_trials
+        data['time/trial'].append(n1)
+        nind = n1/numimgs
+        data['time/image'].append(nind)
+
+    df = pd.DataFrame(data)
+
+    return df
+    
+if __name__ == '__main__':
+    print(CATCH_timer())
