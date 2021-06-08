a709aa9bcf900514e929980082a6f041fb5f6869
laltman2 <laurenealtman@gmail.com>
2021-06-08 11:59:01 -0400

    add device option

--- eval/timer.py
+++ eval/timer.py
@@ -2,17 +2,17 @@ import timeit
 import numpy as np
 import pandas as pd
 
-def CATCH_timer(est_config='overlaps', loc_config='small_noisy', numimgs=200, num_trials=10, loc_dataset='../datasets/yolo_noisy/eval/', est_dataset='../datasets/overlaps/eval/'):
+def CATCH_timer(est_config='overlaps', loc_config='small_noisy', numimgs=200, num_trials=10, loc_dataset='../datasets/yolov5_test/eval/', est_dataset='../datasets/overlaps/eval/', device='cuda'):
 
     modelstart = '''
 from CATCH.Estimator_Torch import Estimator
 from CATCH.Localizerv5_pip import Localizer
 from CATCH.CATCHobject import CATCH
     
-est = Estimator(configuration='{}')
-loc = Localizer(configuration='{}')
+est = Estimator(configuration='{}', device='{}')
+loc = Localizer(configuration='{}', device='{}')
 catch = CATCH(localizer=loc, estimator = est)
-'''.format(est_config, loc_config)
+'''.format(est_config, device, loc_config, device)
     
     imgload = '''
 import cv2
@@ -31,6 +31,7 @@ for n in range({}):
     strpairs = [(loc_setup, loc_stmt), (est_setup, est_stmt), (loc_setup, catch_stmt)]
 
     data = {'model': ['localizer', 'estimator', 'catch'],
+            'device': [device]*3,
             'num_images': [numimgs]*3, 'num_trials': [num_trials]*3,
             'total_time':[], 'time/trial':[], 'time/image':[]}
     
