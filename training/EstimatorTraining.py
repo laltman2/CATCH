447c91887f509689153d66d13a91e394c86ff4e8
laltman2 <laurenealtman@gmail.com>
2021-12-16 14:06:40 -0500

    fixed indents and syntax

--- training/EstimatorTraining.py
+++ training/EstimatorTraining.py
@@ -1,4 +1,7 @@
-import cupy as cp
+try:
+	import cupy as cp
+except:
+	print('falling back to CPU')
 import torch.optim as optim
 import torch.nn as nn
 from torch_estimator_arch import TorchEstimator
@@ -40,13 +43,14 @@ class EstimatorTraining(object):
 		self.epoch = 0
 
 		#same for every model. make settable?
-		self.optimizer = optim.RMSprop(net.parameters(), lr=1e-5)
+		self.optimizer = None
 		self.criterion = nn.SmoothL1Loss()
 
 		if self.config:
 			self.do_setup()
 			self.is_setup = True
 
+	'''
 	@property
 	def config(self):
 		return self.config
@@ -60,32 +64,35 @@ class EstimatorTraining(object):
 	@property
 	def model(self):
 		return self.model
+	'''
 
 	def do_setup(self):
 		makedata(config)
 
 		if config['training']['continue']:
 			print('Searching for model checkpoint')
-		    try:
-		        loadpath = config['training']['savefile'] + '_checkpoints/best.pt'
-		        self.model, self.optimizer = load_checkpoint(loadpath)
-		        print('Checkpoint loaded')
-		    except:
-		        print('No checkpoint found, creating a new model')
+			try:
+				loadpath = config['training']['savefile'] + '_checkpoints/best.pt'
+				self.model, self.optimizer = load_checkpoint(loadpath)
+				print('Checkpoint loaded')
+			except:
+				print('No checkpoint found, creating a new model')
 		else:
 			self.model = TorchEstimator()
 			print('Creating a new model')
 
-        self.model.train()
+		self.model.train()
 
-        #use GPU if you have one
+		self.optimizer = optim.RMSprop(self.model.parameters(), lr=1e-5)
+		
+		#use GPU if you have one
 		if torch.cuda.device_count():
-		    print('Using GPU')
-		    self.device = 'cuda:0'
-		    self.model.to(device)
+			print('Using GPU')
+			self.device = 'cuda:0'
+			self.model.to(device)
 		else:
-		    print('No CUDA device detected, falling back to CPU')
-		    self.device = 'cpu'
+			print('No CUDA device detected, falling back to CPU')
+			self.device = 'cpu'
 
 		#define data loaders here
 		train_set = EstimatorDataset(self.config, settype='train')
@@ -98,11 +105,11 @@ class EstimatorTraining(object):
 
 		self.weights_dir = self.config['training']['savefile'] + '_checkpoints/'
 		if not os.path.isdir(self.weights_dir):
-		    os.mkdir(self.weights_dir)
+			os.mkdir(self.weights_dir)
 
 		self.cfg_path = self.config['training']['savefile'] +'.json'
-		with open(cfg_path, 'w') as f:
-		    json.dump(self.config, f)
+		with open(self.cfg_path, 'w') as f:
+			json.dump(self.config, f)
 
 		self.train_info_path = self.config['training']['savefile']+'_train_info.csv'
 
@@ -111,19 +118,17 @@ class EstimatorTraining(object):
 
 
 	def loss_fn(self, outputs, labels):
-	    z1, a1, n1 = torch.transpose(outputs,0,1)
-	    z2, a2, n2 = torch.transpose(labels,0,1)
+		z1, a1, n1 = torch.transpose(outputs,0,1)
+		z2, a2, n2 = torch.transpose(labels,0,1)
 
-	    loss1 = self.criterion(z1, z2)
-	    loss2 = self.criterion(a1, a2)
-	    loss3 = self.criterion(n1, n2)
+		loss1 = self.criterion(z1, z2)
+		loss2 = self.criterion(a1, a2)
+		loss3 = self.criterion(n1, n2)
 
-	    return loss1,loss2,loss3
+		return loss1,loss2,loss3
 
 	def _train_auto(self):
-		self.stoppers = {'z_stopper': EarlyStopping(),
-						'a_stopper': EarlyStopping(),
-						'n_stopper': EarlyStopping()}
+		self.stoppers = {'z_stopper': EarlyStopping(),'a_stopper': EarlyStopping(), 'n_stopper': EarlyStopping()}
 
 		while not np.all([x.early_stop for x in self.stoppers.values()]):
 			print('Epoch {}'.format(self.epoch))
@@ -131,201 +136,201 @@ class EstimatorTraining(object):
 			#training loop
 			self.model.train()
 			running_loss = 0.0
-		    rzloss = 0.0
-		    raloss = 0.0
-		    rnloss = 0.0
-		    for i, data in enumerate(self.trainloader, 0):
-		        self.optimizer.zero_grad()
+			rzloss = 0.0
+			raloss = 0.0
+			rnloss = 0.0
+			for i, data in enumerate(self.trainloader, 0):
+				self.optimizer.zero_grad()
 
-		        images, scales, labels = data
+				images, scales, labels = data
 
-		        if self.device != 'cpu':
-		            images = images.to(self.device)
-		            scales = scales.to(self.device)
-		            labels = labels.to(self.device)
+				if self.device != 'cpu':
+					images = images.to(self.device)
+					scales = scales.to(self.device)
+					labels = labels.to(self.device)
 
-		        outputs = self.model(images, scales)
+				outputs = self.model(images, scales)
 
-		        paramloss = self.loss_fn(outputs, labels)
-		        zl, al, nl = paramloss
-		        rzloss += zl.item()
-		        raloss += al.item()
-		        rnloss += nl.item()
+				paramloss = self.loss_fn(outputs, labels)
+				zl, al, nl = paramloss
+				rzloss += zl.item()
+				raloss += al.item()
+				rnloss += nl.item()
 
-		        loss = sum(list(paramloss))
-		        loss.backward()
-		        self.optimizer.step()
+				loss = sum(list(paramloss))
+				loss.backward()
+				self.optimizer.step()
 
-		        running_loss += loss.item()
-		        print('Batch {}/{}. Loss: {}'.format(i, len(self.trainloader), loss.item()), end='\r')
-
-		    #evaluation loop
+				running_loss += loss.item()
+				print('Batch {}/{}. Loss: {}'.format(i, len(self.trainloader), loss.item()), end='\r')
+				
+			#evaluation loop
 			self.model.eval()
 			running_tloss = 0.0
-		    ztloss = 0.0
-		    atloss = 0.0
-		    ntloss = 0.0
-		    with torch.no_grad():
-		    	for i, data in enumerate(self.testloader, 0):
-		    		images, scales, labels = data
+			ztloss = 0.0
+			atloss = 0.0
+			ntloss = 0.0
+			with torch.no_grad():
+				for i, data in enumerate(self.testloader, 0):
+					images, scales, labels = data
 
-		    		if self.device != 'cpu':
-		                images = images.to(self.device)
-		                scales = scales.to(self.device)
-		                labels = labels.to(self.device)
+					if self.device != 'cpu':
+						images = images.to(self.device)
+						scales = scales.to(self.device)
+						labels = labels.to(self.device)
 
-		            outputs = self.model(images, scales)
+					outputs = self.model(images, scales)
 
-		            param_tloss = self.loss_fn(outputs, labels)
-		            zl, al, nl = param_tloss
-		            ztloss += zl.item()
-		            atloss += al.item()
-		            ntloss += nl.item()
-		            tloss = sum(list(param_tloss))
+					param_tloss = self.loss_fn(outputs, labels)
+					zl, al, nl = param_tloss
+					ztloss += zl.item()
+					atloss += al.item()
+					ntloss += nl.item()
+					tloss = sum(list(param_tloss))
+			    
+					running_tloss += tloss.item()
 
-		            running_tloss += tloss.item()
+			#update best checkpoint
+			if running_tloss < self.best_loss:
+				self.best_loss = running_tloss
+				self.best_state_checkpoint = {'state_dict': self.model.state_dict(),
+							      'optimizer' : self.optimizer.state_dict()}
 
-		    #update best checkpoint
-		    if running_tloss < self.best_loss:
-        		self.best_loss = running_tloss
-        		self.best_state_checkpoint = {'state_dict': self.model.state_dict(),
-                                 'optimizer' : self.optimizer.state_dict()}
+			#update losses
+			epoch_loss = running_loss / len(self.trainloader)
+			epoch_tloss = running_tloss / len(self.testloader)
+			print('Train loss: {}, test loss: {}'.format('%.3f'%epoch_loss, '%.3f'%epoch_tloss))
 
-            #update losses
-            epoch_loss = running_loss / len(self.trainloader)
-    		epoch_tloss = running_tloss / len(self.testloader)
-    		print('Train loss: {}, test loss: {}'.format('%.3f'%epoch_loss, '%.3f'%epoch_tloss))
+			self.losses['train_loss'].append(epoch_loss)
+			self.losses['test_loss'].append(epoch_tloss)
+			self.losses['z_loss'].append(rzloss/len(self.trainloader))
+			self.losses['z_testloss'].append(ztloss/len(self.testloader))
+			self.losses['a_loss'].append(raloss/len(self.trainloader))
+			self.losses['a_testloss'].append(atloss/len(self.testloader))
+			self.losses['n_loss'].append(rnloss/len(self.trainloader))
+			self.losses['n_testloss'].append(ntloss/len(self.testloader))
 
-    		self.losses['train_loss'].append(epoch_loss)
-    		self.losses['test_loss'].append(epoch_tloss)
-    		self.losses['z_loss'].append(rzloss/len(self.trainloader))
-    		self.losses['z_testloss'].append(ztloss/len(self.testloader))
-    		self.losses['a_loss'].append(raloss/len(self.trainloader))
-    		self.losses['a_testloss'].append(atloss/len(self.testloader))
-    		self.losses['n_loss'].append(rnloss/len(self.trainloader))
-    		self.losses['n_testloss'].append(ntloss/len(self.testloader))
+			#early stopping
+			self.stoppers['z_stopper'](ztloss/len(self.testloader))
+			self.stoppers['a_stopper'](atloss/len(self.testloader))
+			self.stoppers['n_stopper'](ntloss/len(self.testloader))
 
-    		#early stopping
-    		self.stoppers['z_stopper'](ztloss/len(self.testloader))
-    		self.stoppers['a_stopper'](atloss/len(self.testloader))
-    		self.stoppers['n_stopper'](ntloss/len(self.testloader))
+			if self.stoppers['z_stopper'].early_stop:
+				self.model.freeze_z()
+			if self.stoppers['a_stopper'].early_stop:
+				self.model.freeze_a()
+			if self.stoppers['n_stopper'].early_stop:
+				self.model.freeze_n()
+			if np.any([x.early_stop for x in self.stoppers.values()]):
+				self.model.freeze_shared()
 
-    		if self.stoppers['z_stopper'].early_stop:
-    			self.model.freeze_z()
-    		if self.stoppers['a_stopper'].early_stop:
-    			self.model.freeze_a()
-    		if self.stoppers['n_stopper'].early_stop:
-    			self.model.freeze_n()
-    		if np.any([x.early_stop for x in self.stoppers.values()]):
-    			self.model.freeze_shared()
+			self.epoch += 1
 
-    		self.epoch += 1
+			#checkpoint
+			if self.epoch % self.checkpoint_every == 0:
+				num_state_checkpoint = {'state_dict': self.model.state_dict(),
+							'optimizer' : self.optimizer.state_dict()}
+				numpath = self.weightsdir + 'epoch{}.pt'.format(self.epoch)
+				torch.save(num_state_checkpoint, numpath)
 
-    		#checkpoint
-    		if self.epoch % self.checkpoint_every == 0:
-    			num_state_checkpoint = {'state_dict': self.model.state_dict(),
-                                 'optimizer' : self.optimizer.state_dict()}
-		        numpath = self.weightsdir + 'epoch{}.pt'.format(self.epoch)
-		        torch.save(num_state_checkpoint, numpath)
-
-		        df = pd.DataFrame(data = {'epochs': np.arange(self.epoch), **self.losses})
-		        df.to_csv(self.train_info_path)
-		        print('Saved checkpoint at {} epochs'.format(self.epoch))
+				df = pd.DataFrame(data = {'epochs': np.arange(self.epoch), **self.losses})
+				df.to_csv(self.train_info_path)
+				print('Saved checkpoint at {} epochs'.format(self.epoch))
 
 			
 
-	def _train_maual(self, epochs):
+	def _train_manual(self, epochs):
 		while self.epoch < epochs:
 			print('Epoch {} / {}'.format(self.epoch, epochs))
 
 			self.model.train()
 			running_loss = 0.0
-		    rzloss = 0.0
-		    raloss = 0.0
-		    rnloss = 0.0
-		    for i, data in enumerate(self.trainloader, 0):
-		        self.optimizer.zero_grad()
+			rzloss = 0.0
+			raloss = 0.0
+			rnloss = 0.0
+			for i, data in enumerate(self.trainloader, 0):
+				self.optimizer.zero_grad()
 
-		        images, scales, labels = data
+				images, scales, labels = data
 
-		        if self.device != 'cpu':
-		            images = images.to(self.device)
-		            scales = scales.to(self.device)
-		            labels = labels.to(self.device)
+				if self.device != 'cpu':
+					images = images.to(self.device)
+					scales = scales.to(self.device)
+					labels = labels.to(self.device)
 
-		        outputs = self.model(images, scales)
+				outputs = self.model(images, scales)
 
-		        paramloss = self.loss_fn(outputs, labels)
-		        zl, al, nl = paramloss
-		        rzloss += zl.item()
-		        raloss += al.item()
-		        rnloss += nl.item()
+				paramloss = self.loss_fn(outputs, labels)
+				zl, al, nl = paramloss
+				rzloss += zl.item()
+				raloss += al.item()
+				rnloss += nl.item()
 
-		        loss = sum(list(paramloss))
-		        loss.backward()
-		        self.optimizer.step()
+				loss = sum(list(paramloss))
+				loss.backward()
+				self.optimizer.step()
 
-		        running_loss += loss.item()
-		        print('Batch {}/{}. Loss: {}'.format(i, len(self.trainloader), loss.item()), end='\r')
+				running_loss += loss.item()
+				print('Batch {}/{}. Loss: {}'.format(i, len(self.trainloader), loss.item()), end='\r')
 
-		    #evaluation loop
+			#evaluation loop
 			self.model.eval()
 			running_tloss = 0.0
-		    ztloss = 0.0
-		    atloss = 0.0
-		    ntloss = 0.0
-		    with torch.no_grad():
-		    	for i, data in enumerate(self.testloader, 0):
-		    		images, scales, labels = data
+			ztloss = 0.0
+			atloss = 0.0
+			ntloss = 0.0
+			with torch.no_grad():
+				for i, data in enumerate(self.testloader, 0):
+					images, scales, labels = data
 
-		    		if self.device != 'cpu':
-		                images = images.to(self.device)
-		                scales = scales.to(self.device)
-		                labels = labels.to(self.device)
+					if self.device != 'cpu':
+						images = images.to(self.device)
+						scales = scales.to(self.device)
+						labels = labels.to(self.device)
 
-		            outputs = self.model(images, scales)
+					outputs = self.model(images, scales)
 
-		            param_tloss = self.loss_fn(outputs, labels)
-		            zl, al, nl = param_tloss
-		            ztloss += zl.item()
-		            atloss += al.item()
-		            ntloss += nl.item()
-		            tloss = sum(list(param_tloss))
+					param_tloss = self.loss_fn(outputs, labels)
+					zl, al, nl = param_tloss
+					ztloss += zl.item()
+					atloss += al.item()
+					ntloss += nl.item()
+					tloss = sum(list(param_tloss))
 
-		            running_tloss += tloss.item()
+					running_tloss += tloss.item()
 
-		    #update best checkpoint
-		    if running_tloss < self.best_loss:
-        		self.best_loss = running_tloss
-        		self.best_state_checkpoint = {'state_dict': self.model.state_dict(),
-                                 'optimizer' : self.optimizer.state_dict()}
+			#update best checkpoint
+			if running_tloss < self.best_loss:
+				self.best_loss = running_tloss
+				self.best_state_checkpoint = {'state_dict': self.model.state_dict(),
+							      'optimizer' : self.optimizer.state_dict()}
 
-            #update losses
-            epoch_loss = running_loss / len(self.trainloader)
-    		epoch_tloss = running_tloss / len(self.testloader)
-    		print('Train loss: {}, test loss: {}'.format('%.3f'%epoch_loss, '%.3f'%epoch_tloss))
+			#update losses
+			epoch_loss = running_loss / len(self.trainloader)
+			epoch_tloss = running_tloss / len(self.testloader)
+			print('Train loss: {}, test loss: {}'.format('%.3f'%epoch_loss, '%.3f'%epoch_tloss))
 
-    		self.losses['train_loss'].append(epoch_loss)
-    		self.losses['test_loss'].append(epoch_tloss)
-    		self.losses['z_loss'].append(rzloss/len(self.trainloader))
-    		self.losses['z_testloss'].append(ztloss/len(self.testloader))
-    		self.losses['a_loss'].append(raloss/len(self.trainloader))
-    		self.losses['a_testloss'].append(atloss/len(self.testloader))
-    		self.losses['n_loss'].append(rnloss/len(self.trainloader))
-    		self.losses['n_testloss'].append(ntloss/len(self.testloader))
+			self.losses['train_loss'].append(epoch_loss)
+			self.losses['test_loss'].append(epoch_tloss)
+			self.losses['z_loss'].append(rzloss/len(self.trainloader))
+			self.losses['z_testloss'].append(ztloss/len(self.testloader))
+			self.losses['a_loss'].append(raloss/len(self.trainloader))
+			self.losses['a_testloss'].append(atloss/len(self.testloader))
+			self.losses['n_loss'].append(rnloss/len(self.trainloader))
+			self.losses['n_testloss'].append(ntloss/len(self.testloader))
+			
+			self.epoch += 1
 
-    		self.epoch += 1
+			#checkpoint
+			if self.epoch % self.checkpoint_every == 0:
+				num_state_checkpoint = {'state_dict': self.model.state_dict(),
+							'optimizer' : self.optimizer.state_dict()}
+				numpath = self.weightsdir + 'epoch{}.pt'.format(self.epoch)
+				torch.save(num_state_checkpoint, numpath)
 
-    		#checkpoint
-    		if self.epoch % self.checkpoint_every == 0:
-    			num_state_checkpoint = {'state_dict': self.model.state_dict(),
-                                 'optimizer' : self.optimizer.state_dict()}
-		        numpath = self.weightsdir + 'epoch{}.pt'.format(self.epoch)
-		        torch.save(num_state_checkpoint, numpath)
-
-		        df = pd.DataFrame(data = {'epochs': np.arange(self.epoch), **self.losses})
-		        df.to_csv(self.train_info_path)
-		        print('Saved checkpoint at {} epochs'.format(self.epoch))
+				df = pd.DataFrame(data = {'epochs': np.arange(self.epoch), **self.losses})
+				df.to_csv(self.train_info_path)
+				print('Saved checkpoint at {} epochs'.format(self.epoch))
 
 
 	def train(self):
@@ -351,9 +356,9 @@ class EstimatorTraining(object):
 
 		print('Finished training')
 		last_state_checkpoint = {'state_dict': self.model.state_dict(),
-                         'optimizer' : self.optimizer.state_dict()}
+					 'optimizer' : self.optimizer.state_dict()}
 
-        bestpath = self.weightsdir + 'best.pt'
+		bestpath = self.weightsdir + 'best.pt'
 		lastpath = self.weightsdir + 'last.pt'
 		torch.save(self.best_state_checkpoint, bestpath)
 		torch.save(last_state_checkpoint, lastpath)
@@ -365,8 +370,8 @@ class EstimatorTraining(object):
 
 if __name__ == '__main__':
 	with open('torch_train_config.json', 'r') as f:
-    	config = json.load(f)
+		config = json.load(f)
 
-    est_train = EstimatorTraining(config)
-
-    est_train.train()
+	est_train = EstimatorTraining(config)
+    
+	est_train.train()
