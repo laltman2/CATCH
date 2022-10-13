561713c2bb24228c3e26645a8cbba207afadde2f
davidgrier <david.grier@nyu.edu>
2022-10-13 07:32:56 -0400

    small cleanups

--- Estimator.py
+++ Estimator.py
@@ -10,6 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 
+
 class Estimator(object):
 
     def __init__(self,
@@ -27,7 +28,7 @@ class Estimator(object):
         self.scale = ParamScale(self.config).unnormalize
         self.shape = tuple(self.config['shape'])
         self.transform = trf.Compose([trf.ToTensor(),
-                                      #trf.Grayscale(),
+                                      # trf.Grayscale(),
                                       trf.Resize(self.shape)])
         self.model.eval()
 
@@ -35,7 +36,8 @@ class Estimator(object):
         '''Returns CNN that performs regression on holograms'''
         basedir = os.path.dirname(os.path.abspath(__file__))
         data = self.configuration + '_checkpoints'
-        path = os.path.join(basedir, 'cfg_estimator', data, '{}.pt'.format(self.weights))
+        path = os.path.join(basedir, 'cfg_estimator',
+                            data, f'{self.weights}.pt')
         dev = torch.device(self.device)
         checkpoint = torch.load(path, map_location=dev)
 
@@ -62,12 +64,12 @@ class Estimator(object):
         if len(image.shape) == 2:
             image = np.stack((image,)*3, axis=-1)
         if np.mean(image) != 100:
-            image = np.clip(image/np.mean(image)*100.,0,255)
+            image = np.clip(image/np.mean(image)*100., 0, 255)
         if image.dtype != np.uint8:
             image = image.astype(np.uint8)
-        image = image[:,:,0]
+        image = image[:, :, 0]
         return self.transform(image).unsqueeze(0)
-        
+
     def predict(self, images=[], **kwargs):
         scale_list, image_list = [], []
         for image in images:
@@ -81,7 +83,9 @@ class Estimator(object):
             scale = scale.to(self.device)
 
         with torch.no_grad():
-            predictions = self.model(image=image.float(), scale=scale, **kwargs) #change image.float() to image to revert
+            # change image.float() to image to revert
+            predictions = self.model(image=image.float(),
+                                     scale=scale, **kwargs)
         keys = ['z_p', 'a_p', 'n_p']
         results = [{k: v.item() for k, v in zip(keys, self.scale(p))}
                    for p in predictions]
@@ -90,18 +94,15 @@ class Estimator(object):
 
 if __name__ == '__main__':
     import cv2
-    from matplotlib import pyplot as plt
-    
+
     est = Estimator()
 
     img_file = os.path.join('examples', 'test_image_crop.png')
-    #img = cv2.imread(img_file)[:,:,0]
     img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
-    #plt.imshow(img, cmap='gray')
-    #plt.show()
-    img = cv2.rotate(img, cv2.cv2.ROTATE_180)
-    #plt.imshow(img, cmap='gray')
-    #plt.show()
-    results = est.predict([img])#, save_intermediate=True)
-    
+    # plt.imshow(img, cmap='gray')
+    # plt.show()
+    img = cv2.rotate(img, cv2.ROTATE_180)
+    # plt.imshow(img, cmap='gray')
+    # plt.show()
+    results = est.predict([img])  # save_intermediate=True)
     print(results)
--- Localizer.py
+++ Localizer.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import os
-import time
 import numpy as np
-import warnings
-
-import matplotlib
-backend = matplotlib.get_backend()
 from yolov5 import YOLOv5
-matplotlib.use(backend)
 
 
 class Localizer(YOLOv5):
@@ -45,17 +38,17 @@ class Localizer(YOLOv5):
         self.configuration = configuration
         self.version = version
         if not self.version and self.configuration == 'yolov5_test':
-            self.version=2
-        
-        if not self.version:
-            self.version= ''
+            self.version = 2
 
-        self.shape = shape or [1024, 1280] #change 
-            
+        if not self.version:
+            self.version = ''
+
+        self.shape = shape or [1024, 1280]  # change
+
         basedir = os.path.dirname(os.path.abspath(__file__))
         cfg_version = self.configuration + str(self.version)
         path = (basedir, 'cfg_yolov5', cfg_version, 'weights', 'best.pt')
-        self.model_path = os.path.join(*path)        
+        self.model_path = os.path.join(*path)
         self.threshold = threshold
         self.device = device
         self.load_model()
@@ -64,28 +57,28 @@ class Localizer(YOLOv5):
         x1, y1, x2, y2 = pred[:4]
         w, h = int(x2 - x1), int(y2 - y1)
         H, W = self.shape
-        ext = np.max([w,h])
-        is_edge = [(x2-ext<0), (y2-ext<0), (x1+ext>W), (y1+ext>H)]
+        ext = np.max([w, h])
+        is_edge = [(x2 - ext < 0), (y2 - ext < 0),
+                   (x1 + ext > W), (y1 + ext > H)]
         if np.any(is_edge):
-            edge=True
+            edge = True
             where_cut = [i for i, x in enumerate(is_edge) if x]
-            if where_cut == [0,1]:
-                #top left corner
+            if where_cut == [0, 1]:
+                # top left corner
                 x_p = x2 - ext/2.
                 y_p = y2 - ext/2.
             elif 0 in where_cut:
-                #left edge
+                # left edge
                 x_p = x2 - ext/2.
                 y_p = y1 + ext/2.
             elif 3 in where_cut:
-                #bottom edge
+                # bottom edge
                 x_p = x1 + ext/2.
                 y_p = y1 + ext/2.
             else:
-                #top and right edges
+                # top and right edges
                 x_p = x1 + ext/2.
                 y_p = y2 - ext/2.
-            #warnings.warn("Warning: feature at ({},{}) found near frame edge".format(x_p, y_p))
         else:
             x_p, y_p = (x1 + x2)/2., (y1 + y2)/2.
             edge = False
@@ -108,20 +101,21 @@ class Localizer(YOLOv5):
             list of dicts
             len(predictions): number of detected features
             Each prediction consists of
-            {'label': l, 
+            {'label': l,
              'conf': c,
              'edge': e,
-             'bbox': ((x1, y1), w, h), 
-             'x_p': x, 
+             'bbox': ((x1, y1), w, h),
+             'x_p': x,
              'y_p': y}
             l: str
             c: float between 0 and 1
-            e: bool, False if full crop is possible, True if feature is cut off by frame edge
+            e: bool
+                False if full crop is possible
+                True if feature is cut off by frame edge
             x1, y1: top left corner of bounding box
             w, h: width and height of bounding box
             x, y: centroid position
         '''
-        start = time.time()
         img_list = [x*100. if np.max(x) < 100 else x for x in img_list]
         size = np.max(np.array(img_list).shape[1:3])
         results = self.predict(img_list, size=size)
@@ -145,7 +139,7 @@ class Localizer(YOLOv5):
                                    'bbox': bbox, 'edge': edge})
             predictions.append(prediction)
         return predictions
-            
+
 
 if __name__ == '__main__':
     import cv2
@@ -153,13 +147,16 @@ if __name__ == '__main__':
     from matplotlib import pyplot as plt
     from matplotlib.patches import Rectangle
 
+    backend = matplotlib.get_backend()
+    matplotlib.use(backend)
+
     # Create a Localizer
     localizer = Localizer()
-    
+
     # Read hologram (not normalized)
     img_file = os.path.join('examples', 'test_image_large.png')
     test_img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
-    test_img = 1/100.*test_img
+    test_img = 1/100.*test_img.astype(np.float32)
 
     # Use Localizer to identify features in the hologram
     features = localizer.detect([test_img])[0]
