d59175f34dbaf7837a8befcd632936cb7d4246ea
David Grier <david.grier@nyu.edu>
2022-11-03 06:58:54 -0400

    clean up docstring

--- training/EarlyStopping.py
+++ training/EarlyStopping.py
@@ -1,27 +1,29 @@
-class EarlyStopping:
-    """Early stops the training if validation loss doesn't improve after a given patience.
+class EarlyStopping(object):
+    '''Stops training if validation loss doesn't improve
 
-    Adapted from https://github.com/Bjarten/early-stopping-pytorch/blob/master/pytorchtools.py"""
+    Adapted from
+    https://github.com/Bjarten/early-stopping-pytorch/blob/master/pytorchtools.py
+
+    Properties
+    ----------
+    patience: int
+        How long to wait after last time validation loss improved.
+    delta: float
+        Minimum change that qualifies as an improvement.
+        Default: 0
+    '''
     def __init__(self, patience=100, delta=0):
-        """
-        Args:
-            patience (int): How long to wait after last time validation loss improved.
-                            Default: 7
-            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
-                            Default: 0    
-        """
         self.patience = patience
         self.counter = 0
         self.best_loss = None
         self.early_stop = False
         self.delta = delta
-    def __call__(self, val_loss):
 
+    def __call__(self, val_loss):
         if self.best_loss is None:
             self.best_loss = val_loss
-        elif val_loss > self.best_loss - self.delta:
+        elif val_loss > (self.best_loss - self.delta):
             self.counter += 1
-            #self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
